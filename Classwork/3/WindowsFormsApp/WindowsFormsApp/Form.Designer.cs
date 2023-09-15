38d331fb481f94cf027d2d071b5875464e58cb14
Jane Silver <ivanna.mo091@gmail.com>
2023-09-15 13:32:16 +0300

    Classwork 3 added

---
+++ Classwork/3/WindowsFormsApp/WindowsFormsApp/Form.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.DimGray;
+            this.button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button.ForeColor = System.Drawing.Color.White;
+            this.button.Location = new System.Drawing.Point(144, 96);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(232, 82);
+            this.button.TabIndex = 0;
+            this.button.Text = "CLICK ME";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            this.button.Paint += new System.Windows.Forms.PaintEventHandler(this.button_Paint);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(519, 278);
+            this.Controls.Add(this.button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Button";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ Classwork/3/WindowsFormsApp/WindowsFormsApp/Form.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.LinkLabel;
+
+namespace WindowsFormsApp
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private bool click;
+        private int buttonRadius;
+        private LinearGradientBrush baseBrush;
+        private LinearGradientBrush shadowBrush;
+
+
+        public Form()
+        {
+            InitializeComponent();
+
+
+            click = false;
+
+
+            buttonRadius = 7;
+
+
+            PrivateFontCollection privateFontCollection = new PrivateFontCollection();
+            privateFontCollection.AddFontFile("../../Fonts/Golca SemiBold.otf");
+
+            button.Font = new Font(privateFontCollection.Families[0], 12);
+
+
+            baseBrush = new LinearGradientBrush(button.ClientRectangle, Color.FromArgb(119, 238, 216), Color.FromArgb(158, 171, 228), -315F);
+            shadowBrush = new LinearGradientBrush(button.ClientRectangle, Color.FromArgb(73, 201, 191), Color.FromArgb(107, 123, 208), -315F);
+        }
+
+
+        private GraphicsPath GetFigurePath(Rectangle rect, int radius)
+        {
+            GraphicsPath graphicsPath = new GraphicsPath();
+
+            float curveSize = radius * 2F;
+
+            graphicsPath.StartFigure();
+            graphicsPath.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            graphicsPath.AddArc(rect.Width - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            graphicsPath.AddArc(rect.Width - curveSize, rect.Height - curveSize, curveSize, curveSize, 0, 90);
+            graphicsPath.AddArc(rect.X, rect.Height - curveSize, curveSize, curveSize, 90, 90);
+            graphicsPath.CloseFigure();
+
+            return graphicsPath;
+        }
+
+
+        private void button_Paint(object sender, PaintEventArgs e)
+        {
+            if (!click)
+            {
+                Rectangle baseRectangle = new Rectangle(0, 0, button.Width, button.Height - 10);
+                Rectangle shadowRectangle = new Rectangle(0, button.Height - 10, button.Width, 10);
+
+                e.Graphics.FillRectangle(baseBrush, baseRectangle);
+                e.Graphics.FillRectangle(shadowBrush, shadowRectangle);
+
+
+                Size stringSize = TextRenderer.MeasureText(button.Text, button.Font);
+                e.Graphics.DrawString(button.Text, button.Font, Brushes.White, (button.Width - stringSize.Width) / 2, ((button.Height - 10) - stringSize.Height) / 2);
+
+
+                button.Region = new Region(GetFigurePath(button.ClientRectangle, buttonRadius));
+            }
+            else
+            {
+                e.Graphics.FillRectangle(baseBrush, button.ClientRectangle);
+
+
+                Size stringSize = TextRenderer.MeasureText(button.Text, button.Font);
+                e.Graphics.DrawString(button.Text, button.Font, Brushes.White, (button.Width - stringSize.Width) / 2, ((button.Height) - stringSize.Height) / 2);
+
+
+                button.Region = new Region(GetFigurePath(button.ClientRectangle, buttonRadius));
+            }
+        }
+
+
+        private async void button_Click(object sender, EventArgs e)
+        {
+            if (!click)
+            {
+                click = true;
+
+
+                button.Height -= 10;
+                button.Top += 10;
+
+                button.Refresh();
+                await Task.Delay(100);
+            }
+
+
+            click = false;
+
+
+            button.Height += 10;
+            button.Top -= 10;
+
+            button.Refresh();
+        }
+    }
+}
