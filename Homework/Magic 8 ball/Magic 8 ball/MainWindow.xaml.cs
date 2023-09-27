4cf0bae8134cc871fc8170171c9b62a769f4ca22
Jane Silver <ivanna.mo091@gmail.com>
2023-09-27 23:02:37 +0300

    Homework "Magic 8 ball" added

--- Homework/Magic 8 ball/Magic 8 ball/MainWindow.xaml.cs
+++ Homework/Magic 8 ball/Magic 8 ball/MainWindow.xaml.cs
@@ -20,9 +20,69 @@ namespace Magic_8_ball
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> answers;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+            MouseDown += MainWindow_MouseDown;
+
+
+            answers = new List<string>() { 
+                "Бесспорно", "Предрешено", "Никаких сомнений", "Определённо да", "Можешь быть уверен в этом", 
+                "Мне кажется — «да»", "Вероятнее всего", "Хорошие перспективы", "Знаки говорят — «да»",
+                "Пока не ясно, попробуй снова", "Спроси позже", "Лучше не рассказывать", "Сейчас нельзя предсказать", "Сконцентрируйся и спроси опять",
+                "Даже не думай", "Мой ответ — «нет»", "По моим данным — «нет»", "Перспективы не очень хорошие", "Весьма сомнительно"
+            };
+        }
+
+
+        private void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+
+        private void minimizeBttn_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void closeBttn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+        private void askQuestionBttn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(inputTxtBx.Text))
+            {
+                magicBall8.Visibility = Visibility.Hidden;
+
+
+                answer.Height = 270;
+                answer.Width = 270;
+
+                int i = new Random().Next(0, answers.Count());
+                answerTxtBlck.Text = answers.ElementAt(i).ToString();
+            }
+            else
+            {
+                magicBall8.Visibility = Visibility.Visible;
+
+
+                answer.Height = 186;
+                answer.Width = 186;
+
+                answerTxtBlck.Text = "";
+            }
         }
     }
 }
