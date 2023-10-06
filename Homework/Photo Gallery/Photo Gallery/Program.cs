9c23d5056f956f2c877aab1989698ad8c053a687
Jane Silver <ivanna.mo091@gmail.com>
2023-10-06 13:55:47 +0300

    Classwork 9 added

---
+++ Homework/Photo Gallery/Photo Gallery/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photo_Gallery
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            App app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
