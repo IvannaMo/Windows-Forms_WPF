654ba20a1f366725ecb24e29283567f5d8bce648
Jane Silver <ivanna.mo091@gmail.com>
2023-10-06 23:49:41 +0300

    Homework "Photo Gallery" added

--- Homework/Photo Gallery/Photo Gallery/MainWindow.xaml.cs
+++ Homework/Photo Gallery/Photo Gallery/MainWindow.xaml.cs
@@ -20,9 +20,35 @@ namespace Photo_Gallery
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> imagePaths;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+            imagePaths = new List<string>() { "/Images/Programming.jpg", "/Images/Internet.jpg", "/Images/GameDev.jpg", "/Images/Thinking.jpg", "/Images/Animation.gif" };
+
+
+            Random random = new Random();
+
+            List<int> randomList = new List<int>();
+            for (int i = 0; i < imagePaths.Count(); i++)
+            {   
+                int num = random.Next(0, imagePaths.Count() - 1);
+                if (!randomList.Contains(num))
+                {
+                    randomList.Add(num);
+                }
+            }
+            
+
+            tabItemImg1.Source = new BitmapImage(new Uri(imagePaths[randomList[0]], UriKind.Relative));
+            tabItemImg2.Source = new BitmapImage(new Uri(imagePaths[randomList[1]], UriKind.Relative));
+            tabItemImg3.Source = new BitmapImage(new Uri(imagePaths[randomList[2]], UriKind.Relative));
+            //tabItemImg4.Source = new BitmapImage(new Uri(imagePaths[randomList[3]], UriKind.Relative));
+            //tabItemImg5.Source = new BitmapImage(new Uri(imagePaths[randomList[4]], UriKind.Relative));
         }
     }
 }
