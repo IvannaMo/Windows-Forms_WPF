b0c06be220ad3a815e13a02cc466ad53d387ab75
Jane Silver <ivanna.mo091@gmail.com>
2023-10-13 23:54:56 +0300

    Homework "Loan Calculator" added

---
+++ Homework/Loan Calculator/Loan Calculator/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Loan_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void salaryTxtBx_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!int.TryParse(e.Text, out _) && e.Text != ".")
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void salaryTxtBx_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (String.IsNullOrEmpty(salaryTxtBx.Text))
+            {
+                slider.Value = 0;
+            }
+        }
+    }
+}
---
+++ Homework/Loan Calculator/Loan Calculator/Property.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Loan_Calculator
+{
+    internal class Property : FrameworkElement
+    {
+        public static DependencyProperty DataProperty;
+
+
+        static Property()
+        {
+            DataProperty = DependencyProperty.Register("Data", typeof(int), typeof(Property));
+        }
+
+
+        public int Data
+        {
+            get
+            {
+                return (int)GetValue(DataProperty);
+            }
+            set
+            {
+                SetValue(DataProperty, value);
+            }
+        }
+    }
+}
