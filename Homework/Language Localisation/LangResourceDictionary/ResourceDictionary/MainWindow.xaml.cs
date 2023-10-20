a0c2bba30dea40e32fa774bcca3c72f12aa2a22c
Jane Silver <ivanna.mo091@gmail.com>
2023-10-20 20:48:20 +0300

    Homework "Language Localisation" added

---
+++ Homework/Language Localisation/LangResourceDictionary/ResourceDictionary/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LangResourceDictionary
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ResourceDictionary enDictionary;
+        private ResourceDictionary uaDictionary;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            enDictionary = new ResourceDictionary();
+            enDictionary.Source = new Uri("Resources/Lang EN.xaml", UriKind.Relative);
+
+            uaDictionary = new ResourceDictionary();
+            uaDictionary.Source = new Uri("Resources/Lang UA.xaml", UriKind.Relative);
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.Resources.MergedDictionaries[0] == uaDictionary)
+            {
+                Application.Current.Resources.MergedDictionaries[0] = enDictionary;
+            }
+            else
+            {
+                Application.Current.Resources.MergedDictionaries[0] = uaDictionary;
+            }
+        }
+    }
+}
---
+++ Homework/Language Localisation/LangResources/LangResources/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using LangResources.Resources;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LangResources
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CultureInfo cultureInfoEN;
+        private CultureInfo cultureInfoUK;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            cultureInfoEN = new CultureInfo("en");
+            cultureInfoUK = new CultureInfo("uk");
+        }
+
+
+        private void UpdateUI()
+        {
+            nameTxtBlck.Text = lang.NameTxtBlck;
+            surnameTxtBlck.Text = lang.SurnameTxtBlck;
+            phoneTxtBlck.Text = lang.PhoneTxtBlck;
+            okBttn.Content = lang.OkBttn;
+            cancelBttn.Content = lang.CancelBttn;
+            changeLangBttn.Content = lang.ChangeLangBttn;
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Thread.CurrentThread.CurrentUICulture == cultureInfoUK)
+            {
+                Thread.CurrentThread.CurrentUICulture = cultureInfoEN;
+            }
+            else
+            {
+                Thread.CurrentThread.CurrentUICulture = cultureInfoUK;
+            }
+
+            UpdateUI();
+        }
+    }
+}
